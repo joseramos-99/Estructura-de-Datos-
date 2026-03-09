e9a389fa34c54914cb27540c3c221f91b801fda2
joseramos-99 <a2025111096@estudiantes.upsa.edu.bo>
2026-03-09 13:31:03 -0400

    Create Triangulo.h

---
+++ Triangulo.h
@@ -0,0 +1,14 @@
+class Triangulo
+{
+	// Atributos - Variables
+private:
+	float base;
+	float altura;
+	float area;
+	//Metodos - Funciones
+public:
+	Triangulo(void); //Constructor - asigna memoria 
+	~Triangulo(void); //Deestructor - libera memoria
+	//Prototipos
+	float calculararea(float b, float h);
+};
